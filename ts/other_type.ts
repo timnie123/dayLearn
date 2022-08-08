52fde3839c00024a19292ff03caeddd9af16bbba
timnie <spurs_nie@163.com>
2022-08-09 00:37:24 +0800

    类型补充

--- ts/other_type.ts
+++ ts/other_type.ts
@@ -172,3 +172,56 @@ type PartProp = Pick<IInfo, "name">
 let res6:PartProp = {
     name: "Tim"
 }
+
+// Required<Type>
+// 构建一个由 Type 的所有属性组成的类型，设置为必填。与 Partial 相反
+interface IPerson4 {
+    name?: string;
+    age?: number;
+}
+
+let res7:IPerson4 = {
+    name: "Tim",
+}
+
+let res8: Required<IPerson4> = {
+    name: "Tim",
+    age: 18
+}
+
+// Omit<Type, Keys>
+// 通过从 Type 中选取所有属性，然后删除 Keys （属性名或属性名的联合）来构造一个类型。
+interface Student {
+    name: string;
+    age: number
+    score: number;
+    sex: boolean;
+}
+
+type SProps = Omit<Student, "name">
+
+let res9:SProps = {
+    age: 18,
+    score: 100,
+    sex: false
+}
+
+
+// OmitThisParameter
+// 从类型T中剔除this参数类型，并构造一个新类型
+
+function add(x: number): void {
+    console.log(x)
+}
+
+function f0(this: object, x: number) {}
+function f1(x: number) {}
+
+// (x: number) => void
+type T0 = OmitThisParameter<typeof f0>;
+
+// (x: number) => void
+type T1 = OmitThisParameter<typeof f1>;
+
+// string
+type T2 = OmitThisParameter<string>;
